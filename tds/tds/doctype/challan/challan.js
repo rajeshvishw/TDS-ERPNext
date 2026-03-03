803fa6a95ba1233cd4ceddfaf24b29de10a72ee3
Erpnext <erpnext@vmi2573187.contaboserver.net>
2026-03-03 19:32:49 +0100

    added some changes

---
+++ tds/tds/doctype/challan/challan.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Rajesh Vishwakarma and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Challan", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ tds/tds/doctype/challan/challan.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Rajesh Vishwakarma and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Challan(Document):
+	pass
